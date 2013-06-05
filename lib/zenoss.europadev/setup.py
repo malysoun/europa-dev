264a092d80d04217d7024b85d374a396c26a710a
Ian McCracken <ian.mccracken@gmail.com>
2013-06-05 13:56:53 -0500

    Europa-dev egg for utility scripts and whatnot

---
+++ lib/zenoss.europadev/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+long_description = open('README.txt').read()
+
+setup(name='zenoss.europadev',
+      version=version,
+      description="",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='https://github.com/zenoss/europa-dev',
+      license='private',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zenoss'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
