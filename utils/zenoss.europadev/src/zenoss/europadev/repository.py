cc0d4dda1e7b957c1e35b0daea2ec57268e18c8d
Seth Cleveland <scleveland@zenoss.com>
2013-06-07 11:36:34 -0500

    Added purge, fixed clone, added cwd to execute, added repo helpers

--- utils/zenoss.europadev/src/zenoss/europadev/repository.py
+++ utils/zenoss.europadev/src/zenoss/europadev/repository.py
@@ -1,15 +1,69 @@
 import os
 import re
+from functools import reduce, total_ordering
+from operator import not_
 
+def compose(*functions):
+    return reduce(lambda f, g: lambda x: f(g(x)), functions)
+
+@total_ordering
 class Configuration(object):
-    def __init__(self, local, remote):
+    def __init__(self, base, local, remote):
+        self.base = base
         self.local = local
         self.remote = remote
 
     def __repr__(self):
-        return "configuration({0},{1})".format( self.local, self.remote)
+        t = self.tuple()
+        return "Configuration({0},{1},{2})".format( *t)
 
+    def __eq__(self, that):
+        return self.tuple() == that.tuple()
+
+    def __lt__(self, that):
+        return self.tuple() < that.tuple()
+
+    def tuple(self):
+        return (self.base, self.local, self.remote)
+
+    def exists(self):
+        """ Does the local path exists """
+        return os.path.exists( self.localpath())
+
+    def localpath( self):
+        return os.path.join( self.base, self.local)
+
+@total_ordering
 class Configurations(object):
+    def __init__(self, values=None):
+        if values:
+            self.values = values
+        else:
+            self.values = []
+        self.values = set( values)
+
+    def __eq__(self, that):
+        return self.values == that.values
+
+    def __lt__(self, that):
+        return self.values < that.values
+
+    def __repr__(self):
+        return "Configurations( {0})".format( self.values)
+
+    def __iter__(self):
+        return iter(self.values)
+
+    def exist(self):
+        """ find all configurations that exist locally """
+        exists = filter( Configuration.exists, self.values)
+        return Configurations(exists)
+
+    def not_exist(self):
+        """ find all configurations that exist locally """
+        not_exists = filter( compose( not_, Configuration.exists), self.values)
+        return Configurations( not_exists)
+
     @staticmethod
     def get():
         base_path = os.path.join( __file__, '..', '..', '..', '..', '..', '..')
@@ -21,6 +75,6 @@ class Configurations(object):
             for line in f.readlines():
                 line = line.strip()
                 c = re.split( "\s*,\s*", line) 
-                c = Configuration( *c)
+                c = Configuration(base_path, *c)
                 configs.append( c)
-        return configs
+        return Configurations( configs)
