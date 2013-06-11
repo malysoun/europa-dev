0ed09b8f83a5ac1690b00da17c2c1da970c5479a
Seth Cleveland <scleveland@zenoss.com>
2013-06-11 14:05:21 -0500

    parse private repo config, updated .gitignore to handle directories

--- utils/zenoss.europadev/src/zenoss/europadev/repository.py
+++ utils/zenoss.europadev/src/zenoss/europadev/repository.py
@@ -10,8 +10,8 @@ def compose(*functions):
 
 @total_ordering
 class Configuration(object):
-    def __init__(self, base, local, remote):
-        self.base = base
+    def __init__(self, root, local, remote):
+        self.root = root
         self.local = local
         self.remote = remote
 
@@ -26,7 +26,7 @@ class Configuration(object):
         return self.tuple() < that.tuple()
 
     def tuple(self):
-        return self.base, self.local, self.remote
+        return self.root, self.local, self.remote
 
     def exists(self):
         """ Does the local path exists """
@@ -37,7 +37,7 @@ class Configuration(object):
         return self.local
 
     def localpath( self):
-        return os.path.join(self.base, self.local)
+        return os.path.join(self.root, self.local)
 
     def remotepath( self):
         return self.remote
@@ -80,16 +80,34 @@ class Configurations(object):
         return reduce(func, self.values, init)
 
     @staticmethod
-    def get():
-        base_path = os.path.join(__file__, '..', '..', '..', '..', '..', '..')
-        base_path = os.path.realpath(base_path)
-        conf_path = os.path.join(base_path, 'repos')
+    def root():
+        path = os.path.join(__file__, '..', '..', '..', '..', '..', '..')
+        path = os.path.realpath(path)
+        return path
 
+    @staticmethod
+    def parse(root, file):
         configs = []
-        with open(conf_path, 'r') as f:
+        with open(file, 'r') as f:
             for line in f.readlines():
                 line = line.strip()
                 c = re.split("\s*,\s*", line)
-                c = Configuration(base_path, *c)
+                c = Configuration(root, *c)
                 configs.append(c)
+        return configs
+
+    @staticmethod
+    def get():
+        root = Configurations.root()
+        conf_file = os.path.join(root, 'repos')
+
+        # load public repos configuration
+        configs = Configurations.parse(root, conf_file)
+
+        # load private repos configuration
+        private_conf_file = os.path.join(root, 'private', 'repos')
+        if os.path.exists(private_conf_file):
+            private_configs = Configurations.parse(root, private_conf_file)
+            configs.extend(private_configs)
+
         return Configurations(configs)
