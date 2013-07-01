0e750f72992d95a30af0264cab6103a45086b3d9
Ian McCracken <ian@tintin.local>
2013-07-01 09:37:19 -0500

    Update update

--- utils/zenoss.europadev/src/zenoss/europadev/update.py
+++ utils/zenoss.europadev/src/zenoss/europadev/update.py
@@ -1,9 +1,12 @@
-from .git import clone
+from .git import clone, say, warn, debug
 from .repository import Configurations
 from .termutils import *
 
 
 def main():
+    debug.wrap = False
+    say.wrap = False
+    warn.wrap = False
     root = Configurations.get().root()
     shell("git pull", cwd=root)
     clone().perform(None)
