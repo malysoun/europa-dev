769abdd24592448b152f40086b33aff42a6357be
Ian McCracken <ian.mccracken@gmail.com>
2013-07-26 16:47:41 -0500

    Updated dependencies, made guest additions work

--- vagrant/packer/scripts/virtualbox.sh
+++ vagrant/packer/scripts/virtualbox.sh
@@ -1,11 +1,11 @@
 cd /tmp
 mkdir -p /mnt/vbox
-if [ -f /home/vagrant/VBoxGuest*.iso ]
+if [ -f /home/zendev/VBoxGuest*.iso ]
 then
 
-    mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/vbox
+    mount -o loop /home/zendev/VBoxGuest*.iso /mnt/vbox
     sh /mnt/vbox/VBoxLinuxAdditions.run
     umount /mnt/vbox
     rmdir /mnt/vbox
-    rm -rf /home/vagrant/VBoxGuest*.iso
+    rm -rf /home/zendev/VBoxGuest*.iso
 fi
